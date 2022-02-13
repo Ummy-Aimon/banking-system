e7481a55a9daa1fca7806eeb9e90de8cf0fff30a
Ummy-Aimon <ummyatia@gmail.com>
2022-02-13 19:03:39 +0600

    error-handling

--- banking.js
+++ banking.js
@@ -1,23 +1,26 @@
 function deposite(){
     // deposite Money
-    
     const deposite= document.getElementById("deposite-value");
-    const depositeValue= deposite.value;
-    const depositeOutput= document.getElementById("deposite-Output");
+    const depositeValue= parseFloat(deposite.value);
+    if(depositeValue>0){
+      const depositeOutput= document.getElementById("deposite-Output");
     depositeOutput.innerText=  Number( depositeOutput.innerText) + Number(depositeValue);
-    deposite.value= " ";
-
+    deposite.value= "";
+    
 // Total Balance Money
 
   const BalanceOutput= document.getElementById("Banlance-Output");
   BalanceOutput.innerText= Number (BalanceOutput.innerText) + Number (depositeValue);
+    }
+    
 
 }
 //   windraw Money
 
 function windraw(){
 const windraw= document.getElementById("windraw-value");
-const windrawValue= windraw.value;
+const windrawValue= parseFloat (windraw.value);
+if(windrawValue>0){
 const WindrawOutput= document.getElementById("Windraw-Output");
 WindrawOutput.innerText= Number( WindrawOutput.innerText) + Number (windrawValue)
 windraw.value=" ";
@@ -26,5 +29,5 @@ windraw.value=" ";
 
 const BalanceOutput= document.getElementById("Banlance-Output");
 BalanceOutput.innerText= Number (BalanceOutput.innerText)- Number (windrawValue) ;   
-    
+}   
 }
